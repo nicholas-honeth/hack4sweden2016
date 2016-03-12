0dd567a3e00c5d8356ad298810f27d770d86d984
Nicholas Honeth <honeth@kth.se>
2016-03-12 15:16:02 +0100

    Weatherstations loading values from data frame

--- WeatherStations.R
+++ WeatherStations.R
@@ -9,11 +9,12 @@ library("rgeos")
 library("OpenStreetMap")
 library("ggplot2")
 
-## Inspiration from: 
+## Inspiration from:
 ## http://www.maths.lancs.ac.uk/~rowlings/Teaching/UseR2012/crime.html
 
-##Fetch SMHI JSON data for rainfall in the last hour
-hourlyFrame <- fromJSON("http://opendata-download-metobs.smhi.se/api/version/latest/parameter/7/station-set/all/period/latest-hour/data.json", simplifyVector = TRUE)
+
+##Fetch SMHI JSON data for wind speed in the last hour
+hourlyFrame <- fromJSON("http://opendata-download-metobs.smhi.se/api/version/latest/parameter/4/station-set/all/period/latest-hour/data.json", simplifyVector = TRUE)
 
 ## Convert to dataframe
 weatherStations <- do.call("rbind.fill", lapply(hourlyFrame, as.data.frame))
@@ -25,23 +26,33 @@ weatherStations <- weatherStations[is.na(weatherStations$longitude) == FALSE,]
 coordinates(weatherStations) = ~longitude + latitude
 
 ## Test plot - this will show the wrong projection though
-plot(weatherStations)
+#plot(weatherStations)
 
 ## Set the correct WGS84 coordinate reseternce system (TODO: Check up on exactly what SMHI uses)
 proj4string(weatherStations) = CRS("+init=epsg:4326")
 
 ## Test plot again - now the shape of Sweden should be clearer
-plot(weatherStations)
+#plot(weatherStations)
 
 ## Derive the bounding box from the data
-latLongBox = bbox(spTransform(weatherStations, CRS("+init=epsg:4326")))
+#latLongBox = bbox(spTransform(weatherStations, CRS("+init=epsg:4326")))
 
 ## Fetch OSM data for the region in the bounding box
-map = openmap(c(latLongBox[2, 2], latLongBox[1, 1]), c(latLongBox[2, 1], latLongBox[1, 2]), type = "osm")
+#map = openmap(c(latLongBox[2, 2], latLongBox[1, 1]), c(latLongBox[2, 1], latLongBox[1, 2]), type = "osm")
 
-plot(map)
+#plot(map)
 ## Plot the weather station positions transformed to OSM data
-points(spTransform(weatherStations, osm()))
+#points(spTransform(weatherStations, osm()))
 
-## TODO: Figure out how to reference the data in WeatherStations$value like the "value" fainfall in mm and use it in the plot.
 
+## Pick out entries from "value" to a new list which we populate with the values
+for (i in 1:length(weatherStations)) {  if (!is.null(weatherStations$value[[i]][2])) values[i] <- (as.double(weatherStations$value[[i]][2])) else values[i] <- NA  }
+
+## Add the values as numerics to the weatherStations frames
+weatherStations$values <- as.numeric(values)
+
+library("ggplot2")
+weatherStations@data = cbind(weatherStations@data, coordinates(weatherStations))
+
+
+ggplot(weatherStations@data) + geom_point(aes(x = longitude, y = latitude, colour = round(values))) + coord_equal()
