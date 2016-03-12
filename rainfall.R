425cdfca4df26f5f5546dc3ad51b92c01a672542
Daniel Brodén <danbro@kth.se>
2016-03-12 19:26:56 +0100

    Updates

---
+++ rainfall.R
@@ -0,0 +1,8 @@
+# The purpose of this script is to generate a data frame of rainfall timeseries
+# averaged per regions in Sweden
+# Expected data frame is n observations and 22 columns
+# 21 columns represent the different regions in Sweden
+# 1 column represents the risk of floods (either 0, 0.2, 0.4, 0.6, 0.8 or 1.0)
+
+
+
