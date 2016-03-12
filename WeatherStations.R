17bdb61deb0b55895a9c73c22631811e9e8bf45c
Nicholas Honeth <honeth@kth.se>
2016-03-12 17:37:26 +0100

    Full example with heatmap from SMHI wind speed

--- WeatherStations.R
+++ WeatherStations.R
@@ -56,3 +56,28 @@ weatherStations@data = cbind(weatherStations@data, coordinates(weatherStations))
 
 
 ggplot(weatherStations@data) + geom_point(aes(x = longitude, y = latitude, colour = round(values))) + coord_equal()
+
+#shape <- readOGR(dsn="C:/Users/nicho/Dropbox/Documents/Development/R/Hack4Sweden/clone/hack4sweden2016/Data", layer="lansgranser")
+shape <- readOGR(dsn="Data", layer="lansgranser")
+
+
+## Transforms the projection used in the SCB map to the same projection as SMHI
+shape.epsg <- spTransform(shape, CRS("+init=epsg:4326"))
+
+## Maps the weather stations to the län polygons that they belong to
+stationsPerLan <- over(SpatialPolygons(shape.epsg@polygons), SpatialPoints(weatherStations),returnList = TRUE)
+
+ unlist(lapply(stationsPerLan, length))
+
+
+## Calculate the mena for each län
+meanWind <- list(numeric)
+for(i in 1:length(stationsPerLan)) {meanWind[i] = mean(as.numeric(values[stationsPerLan[[i]]]), na.rm=TRUE)}
+shape$windAvg  <- as.numeric(meanWind)
+
+## Calculate each Län wind as a factor of the maximum windy Län
+shape$windFactor  <- as.numeric(meanWind)/max(as.numeric(values),na.rm = TRUE)
+
+
+## Plot a heat map of 100 stages
+plot(shape, col=heat.colors(100)[round(shape$windFactor*100)])
