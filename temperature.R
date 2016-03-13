f082b908e4b50a14aa259a3e4d065a87b6af6a19
Nicholas Honeth <honeth@kth.se>
2016-03-13 09:38:00 +0100

    Some testing with the temp data and MSB image

--- temperature.R
+++ temperature.R
@@ -6,15 +6,15 @@ library(RCurl)
  stationsdf <- read.csv2(file="Data/weatherStations_location_code.csv",stringsAsFactors = F)
  stationsdf$latlong  <- paste(stationsdf$Latitud,stationsdf$Longitud, sep = ":")
 
- for (i in 1:dim(stationsdf)[1]){
-        URL <- paste("http://opendata-download-metobs.smhi.se/api/version/latest/parameter/2/station/",stationsdf$Stationsnummer[i],"/period/corrected-archive/data.csv",sep="")
-        destfile <- paste("Data/temperature/",stationsdf$Stationsnummer[i], ".csv", sep="")
-        if (!file.exists(destfile)) {
-          if(url.exists(URL)){
-                  download.file(URL,destfile)
-          }
-        }
- }
+ # for (i in 1:dim(stationsdf)[1]){
+ #        URL <- paste("http://opendata-download-metobs.smhi.se/api/version/latest/parameter/2/station/",stationsdf$Stationsnummer[i],"/period/corrected-archive/data.csv",sep="")
+ #        destfile <- paste("Data/temperature/",stationsdf$Stationsnummer[i], ".csv", sep="")
+ #        if (!file.exists(destfile)) {
+ #          if(url.exists(URL)){
+ #                  download.file(URL,destfile)
+ #          }
+ #        }
+ # }
 
 require(tools)
 stationsdf <- read.csv2(file="Data/weatherStations_location_code.csv",stringsAsFactors = F)
@@ -27,6 +27,6 @@ for (i in 1:length(filenames)){
 
 for (i in 1:length(filenames)){
                print(filenames[i])
-               destfile <- paste("Data/rainfall/",filenames[i],".csv",sep="")
-               if (stationsdf$rainfall[stationsdf$Stationsnummer == filenames[i]] != NULL) stationsdf$rainfall[stationsdf$Stationsnummer == filenames[i]] <- read.csv2(file=destfile, stringsAsFactors = F, skip = grep(pattern="Datum",readLines(destfile))-1)
+               destfile <- paste("Data/temperature/",filenames[i],".csv",sep="")
+               if (is.null(stationsdf$temperature[stationsdf$Stationsnummer == filenames[i]])) { stationsdf$temperature[stationsdf$Stationsnummer == filenames[i]] <- as.list(read.csv2(file=destfile, stringsAsFactors = F, skip = grep(pattern="Datum",readLines(destfile))-1)) }
 }
